123b318e5e8694bcc93bd367438bef65283618d4
Ethan <eherman247@gmail.com>
2024-08-23 13:30:07 -0600

    finished group controllers

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const createGroupRoutes = require('./routes/groups')
+const groupRoutes = require('./routes/groups')
 
 // express app
 const app = express()
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.use('/api/groups', createGroupRoutes)
+app.use('/api/groups', groupRoutes)
 
 // connect to db
 mongoose.connect(process.env.ATLAS_URI)
